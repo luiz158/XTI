835b68381a859933c698b3af52347fdfa9e771e3
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2013-07-18 14:40:24 -0300

    Commit inicial do projeto em aula da XTI.

---
+++ xti/src/br/com/xti/logica/CahorroTestExterno.java
@@ -0,0 +1,14 @@
+package br.com.xti.logica;
+
+import br.com.xti.poo.Cachorro;
+
+public class CahorroTestExterno {
+	
+	@SuppressWarnings("unused")
+	public static void main(String[] args) {
+		
+		Cachorro c = new Cachorro();
+		
+	}	
+
+}
