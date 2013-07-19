f53c1be2eadd99e9e178cd206db5b914968f0c6e
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2013-07-19 17:50:07 -0300

    Universidade XTI - JAVA - 045 - Relacionamento tem-um, Composição.

--- xti/src/br/com/xti/poo/Carro.java
+++ xti/src/br/com/xti/poo/Carro.java
@@ -5,14 +5,21 @@ public class Carro {
 	String modelo;
 	int velocidadeMaxima;
 	double segundosZeroACem;
+	Motor motor;
 	
 	public Carro() {
 		
 	}
 
 	public Carro(String modelo, int velocidadeMaxima, double segundosZeroACem) {
+		this(modelo, velocidadeMaxima, segundosZeroACem, null);
+	}
+	
+	public Carro(String modelo, int velocidadeMaxima, double segundosZeroACem, Motor motor) {
 		this.modelo = modelo;
 		this.velocidadeMaxima = velocidadeMaxima;
 		this.segundosZeroACem = segundosZeroACem;
+		this.motor = motor;
 	}
+	
 }
--- xti/src/br/com/xti/poo/CarroTest.java
+++ xti/src/br/com/xti/poo/CarroTest.java
@@ -8,10 +8,19 @@ public class CarroTest {
 		ferrari.modelo = "Ferrari Enzo";
 		ferrari.velocidadeMaxima = 349;
 		ferrari.segundosZeroACem = 3.2;
+		Motor v12 = new Motor();
+		v12.tipo = "V12";
+		v12.potencia = 660;
+		ferrari.motor = v12;	//Adiciona Motor ao Carro.
 		
-		Carro Koenigsegg = new Carro("Koenigsegg CCXR", 430, 3.1);
+		Carro koenigsegg = new Carro("Koenigsegg CCXR", 430, 3.1);
+		Motor v8 = new Motor("V8", 1018);
+		koenigsegg.motor = v8;
 		
-		System.out.println(Koenigsegg.modelo);
+		Carro bugatti = new Carro("Bugatti Veyron", 430, 2.2, new Motor("W16", 1200));
+		
+		System.out.println(bugatti.modelo);
+		System.out.println(bugatti.motor.potencia);
 	}
 
 }
