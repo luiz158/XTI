8291b2b01c22b92ff67b28a243d514e8c517c8da
Marcelo Stadtlober <marcelostadtlober@gmail.com>
2013-07-19 18:24:19 -0300

    Universidade XTI - JAVA - 046 - Enumeração (enum) Constantes Java.

---
+++ xti/src/br/com/xti/poo/Medida.java
@@ -0,0 +1,12 @@
+package br.com.xti.poo;
+
+public enum Medida {
+	
+	MM("Milímetro"), CM("Centímetro"), M("Metro");
+	
+	public String titulo;
+	Medida(String titutlo) {
+		this.titulo = titutlo;		
+	}
+
+}
---
+++ xti/src/br/com/xti/poo/PeçasXadrez.java
@@ -0,0 +1,6 @@
+package br.com.xti.poo;
+
+public enum PeçasXadrez {
+	
+	PEÃO, TORRE, BISPO, CAVALO, REI, RAINHA;
+}
